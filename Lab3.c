b3556d057300d64b0ea2593acbe9c20b1809b627
MuntherAlsaid <Muntheralsaid7@gmail.com>
2024-10-30 19:02:49 +0400

    Add files via upload

---
+++ Lab3.c
@@ -0,0 +1,40 @@
+#include<stdio.h> 
+#include <stdbool.h> 
+#define SIZE 24
+#define nRows 8
+#define nCols 3
+// PROTOTYPES
+void print_array(int array[], int length);
+void print_matrix(int mat[][nCols], int rows);
+bool isValid(const int arr[],int length, int pos);
+
+
+int main()
+{
+int arr[SIZE];
+int arr2d[nRows][nCols];
+print_matrix(arr2d,nRows);
+//Call to different functions
+}
+
+void print_array(int array[], int length){
+   for(int i=0;i<length; i++)
+      printf("array[%d]= %d\n", i, array[i]);
+}
+
+void print_matrix(int mat[][nCols], int rows){
+   for(int i=0;i<rows; i++){
+    for(int j=0;j<nCols; j++)
+         printf("mat[%d][%d]= %d\n", i,j, mat[i][j]);
+    puts("");     
+   }     
+}
+
+//FUNCTION DEFINITIONS
+bool isValid(const int arr[], int length, int pos) {
+    if (pos >= 0 && pos < length) {
+        return true;
+    } else {
+        return false;
+    }
+}
